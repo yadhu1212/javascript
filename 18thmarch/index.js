0af8c3128f1b6d96f2d75292eac28964f876f70e
Yadhukrishna <yk2000.yk45@gmail.com>
2023-03-18 20:27:15 +0530

    learnt about variables

---
+++ 18thmarch/index.js
@@ -0,0 +1,13 @@
+var FirstName = "yadhu"
+var SecondName = " krishna"
+var FullName = FirstName+SecondName 
+// console.log(FullName)
+
+var FirstName = "John"
+var SecondName = " Doe"
+var FullName = FirstName+SecondName  
+const DateOfBirth = 1996
+const CorrentYear = 2023
+var Age = CorrentYear-DateOfBirth
+var Class = 5
+console.log("My name is "+FullName+". " + "My age is " +Age +". "+ "I'am studying in " +Class+"th grade" + ".")
