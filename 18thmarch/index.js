d73cdf39d8da8431d1b8f87d109bc1f332210ad7
Yadhukrishna <yk2000.yk45@gmail.com>
2023-03-19 18:10:24 +0530

    create biodata

--- 18thmarch/index.js
+++ 18thmarch/index.js
@@ -7,7 +7,8 @@ var FirstName = "John"
 var SecondName = " Doe"
 var FullName = FirstName+SecondName  
 const DateOfBirth = 1996
-const CorrentYear = 2023
+const CentYear = 2023
 var Age = CorrentYear-DateOfBirth
 var Class = 5
-console.log("My name is "+FullName+". " + "My age is " +Age +". "+ "I'am studying in " +Class+"th grade" + ".")
+console.log(`My name is ${FullName}. My age is ${Age}. I am studying in ${CClass}th grade.`)
+//console.log()
