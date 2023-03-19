f7a907bd22414b3e0a2f5650b10136d225ba929d
Yadhukrishna <yk2000.yk45@gmail.com>
2023-03-19 20:23:58 +0530

    learned about arithmatic opparetors

---
+++ 19thmarch/operaters/arithmeticOperator.js
@@ -0,0 +1,43 @@
+// Number addition
+{
+ let x = 5 
+let  y = 6
+let Result = x + y
+
+console.log("Result:", Result)
+
+}
+//string addition
+{
+
+    let FirstName = "Yadhu"
+    let WhiteSpace = " "
+    let SecondName = "krishna" 
+    let Result = FirstName + WhiteSpace + SecondName
+    console.log("string addition result:", Result)
+
+}
+ 
+//String and number addition 
+{
+    let String = "Yadhu Krishna"
+    let Number = 2000
+    let Result = String + Number 
+    console.log("String and Number Addition Result:", Result)
+}
+
+// Boolean Addition 
+{
+let booleanTrue = true 
+let booleanFalse = false
+let result = booleanTrue + booleanFalse
+console.log("Boolean addition result:", result)
+}
+
+// Boolean and Number Addition 
+{
+    let boolean = true
+    let number = 21
+    let result = boolean + number  
+    console.log("Boolean and number addition:", result)
+}
